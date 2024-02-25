969d3c9d315a8323fa78206d3554b1a10e4eb59e
manjulat13 <manjulat13@gmail.com>
2024-02-26 00:29:06 +0530

    ME_QA_XGOOGLE_CALC1_MODULE_L1

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,12 +1,20 @@
 package demo;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.v116.indexeddb.model.Key;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+
+import com.google.common.base.Verify;
+
 import java.util.logging.Level;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -33,7 +41,7 @@ public class TestCases {
 
         // Set browser to maximize and wait
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
     }
 
@@ -48,9 +56,148 @@ public class TestCases {
     
     public  void testCase01(){
         System.out.println("Start Test case: testCase01");
-        driver.get("https://www.google.com");
+        driver.get("https://www.google.com/");
+        String currUrl=driver.getCurrentUrl();
+        if(currUrl.contains("google"))
+        {
+            System.out.println("currUrl contains google");
+        }
+        //Type "calculator" in the Google search bar and press Enter.
+
+        WebElement searchBar=driver.findElement(By.id("APjFqb"));
+        searchBar.sendKeys("calculator");
+        searchBar.sendKeys(Keys.ENTER);
+
+        //Verify that the Google Calculator loads.
+        WebElement calculator=driver.findElement(By.xpath("//div[@class='MjjYud']//div[@jscontroller='qxNryb']"));
+        String text=calculator.getText();
+        if(text.contains("Calculator")){
+            System.out.println("Google Calculator loaded");
+        }else{
+            System.out.println("Google Calculator not loaded");
+
+        }
+
+        //Confirm that the initial display shows zero(0)
+        WebElement zero=driver.findElement(By.xpath("//div[@jsname='zLiRgc']/span"));
+        String zeroText=zero.getText();
+        if(zeroText.contains("0")){
+            System.out.println("initial display is zero(0)");
+        }else{
+            System.out.println("initial display is not zero(0)");
+
+        }
         System.out.println("end Test case: testCase02");
     }
+    public void textCase02(){
+        
+        driver.get("https://www.google.com/");
 
+        //Type "calculator" in the Google search bar and press Enter.
+
+        WebElement searchBar=driver.findElement(By.id("APjFqb"));
+        searchBar.sendKeys("calculator");
+        searchBar.sendKeys(Keys.ENTER);
+        //Perform an addition operation, 5 + 7.
+        driver.findElement(By.xpath("//div[@jsname='Ax5wH']")).click();
+        driver.findElement(By.xpath("//div[@aria-label='plus']")).click();
+        driver.findElement(By.xpath("//div[@jsname='rk7bOd']")).click();
+
+        //Confirm that the displayed result is correct (in this case, 12).
+        driver.findElement(By.xpath("//div[@aria-label='equals']")).click();
+        String add=driver.findElement(By.xpath("//div[@jsname='zLiRgc']/span")).getText();
+        if(add.equals("12")){
+            System.out.println("Addition Result is correct");
+        }
+        else{
+            System.out.println("Addition Result is not correct");
+
+        }
+
+        //Perform a subtraction operation, 15 - 8.
+        driver.findElement(By.xpath("//div[@jsname='N10B9']")).click();
+        driver.findElement(By.xpath("//div[@jsname='Ax5wH']")).click();
+
+        driver.findElement(By.xpath("//div[@aria-label='minus']")).click();
+        driver.findElement(By.xpath("//div[@jsname='T7PMFe']")).click();
+        driver.findElement(By.xpath("//div[@aria-label='equals']")).click();
+        
+        //Confirm that the displayed result is correct (in this case, 7).
+        String sub=driver.findElement(By.xpath("//div[@jsname='zLiRgc']/span")).getText();
+        if(sub.equals("7")){
+            System.out.println("Substraction Result is correct");
+        }
+        else{
+            System.out.println("Substraction Result is not correct");
+
+        }
+    }
+    public void testCase03(){
+        driver.get("https://www.google.com/");
+
+        //Type "calculator" in the Google search bar and press Enter.
+
+        WebElement searchBar=driver.findElement(By.id("APjFqb"));
+        searchBar.sendKeys("calculator");
+        searchBar.sendKeys(Keys.ENTER);
+        // Perform a multiplication operation, 10 * 3.
+         
+         driver.findElement(By.xpath("//div[@jsname='N10B9']")).click();
+         driver.findElement(By.xpath("//div[@jsname='bkEvMb']")).click();
+ 
+         driver.findElement(By.xpath("//div[@aria-label='multiply']")).click();
+         driver.findElement(By.xpath("//div[@jsname='KN1kY']")).click();
+         driver.findElement(By.xpath("//div[@aria-label='equals']")).click();
+        
+        // Confirm that the displayed result is correct (in this case, 30).
+        String mul=driver.findElement(By.xpath("//div[@jsname='zLiRgc']/span")).getText();
+        if(mul.equals("30")){
+            System.out.println("Multiplication Result is correct");
+        }
+        else{
+            System.out.println("Multiplication Result is not correct");
+
+        }
+        // Click the "AC" (All Clear) button on the calculator.
+        driver.findElement(By.xpath("//div[@aria-label='all clear']")).click();
+        String clear=driver.findElement(By.xpath("//div[@jsname='zLiRgc']/span")).getText();
+
+        // Verify that the display clears.
+        if(clear.equals("0")){
+            System.out.println("Ac Button Cleared Display");
+        }
+        else{
+            System.out.println("Ac Button not Cleared Display");
+
+        }
+    }
+    public void testCase04(){
+        driver.get("https://www.google.com/");
+
+        //Type "calculator" in the Google search bar and press Enter.
+
+        WebElement searchBar=driver.findElement(By.id("APjFqb"));
+        searchBar.sendKeys("calculator");
+        searchBar.sendKeys(Keys.ENTER);
+
+        //Perform a division operation, 20 / 4.
+        driver.findElement(By.xpath("//div[@jsname='lVjWed']")).click();
+         driver.findElement(By.xpath("//div[@jsname='bkEvMb']")).click();
+ 
+         driver.findElement(By.xpath("//div[@aria-label='divide']")).click();
+         driver.findElement(By.xpath("//div[@jsname='xAP7E']")).click();
+         driver.findElement(By.xpath("//div[@aria-label='equals']")).click();
+         String div=driver.findElement(By.xpath("//div[@jsname='zLiRgc']/span")).getText();
+
+        //Confirm that the displayed result is correct (in this case, 5).
+    
+        if(div.equals("5")){
+            System.out.println("Division Result is Correct");
+        }
+        else{
+            System.out.println("Division Result is Correct");
+
+        }
+    }
 
 }
